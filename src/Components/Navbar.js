d9cb6e06c166295bb3b0dd1042143b753cd30a02
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-05-20 23:16:43 +0530

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from 'react-icons/hi2';
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRounedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
 
 const Navbar = () => {
+
+    const [openMenu,setOpenMenu,] = useState(false);
+    const menuOptions = [
+      {
+        text: "Home",
+        icon: <HomeIcon/>
+      },
+      {
+        text: "About",
+        icon: <InfoIcon/>
+      },
+      {
+        text: "Testimonials",
+        icon: <CommentRoundedIcon/>
+      },
+      {
+        text: "Contact",
+        icon: <PhoneRounedIcon/>
+      },
+      {
+        text: "Cart",
+        icon: <ShoppingCartRoundedIcon/>
+      },
+    ];
+    
   return (
     <div>Navbar</div>
   )

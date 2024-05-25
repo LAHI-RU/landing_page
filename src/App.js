822904cab5bf99540b468c32b540ad37625816fd
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-05-25 16:00:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
+import Work from './Components/Work';
+import Testimonial from './Components/Testimonial';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
     <div className="App">
         <Home/>
         <About/>
+        <Work/>
+        <Testimonial/>
+        <Contact/>
+        <Footer/>
     </div>
   );
 }
